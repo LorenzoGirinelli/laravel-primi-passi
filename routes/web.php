6cfeed589ec2b6cd3b86f7e30e18f8925d6e584b
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2022-02-08 17:40:19 +0100

    finito

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $data = [
+        'name' => 'Lorenzo',
+        'eta' => '22'
+    ];
+
+    return view('home',$data);
+})->name('home');
+
+Route::get('/chi-siamo', function () {
+    return view('about');
+})->name('about');
